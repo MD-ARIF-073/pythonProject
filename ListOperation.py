ff2d80cb39757c2c4e6bada626549a90ab4ab5b5
Mohammad Arif Ishtiaq <arifsani73@gmail.com>
2024-12-09 18:20:23 +0600

    Implemented some list opration in python

---
+++ ListOperation.py
@@ -0,0 +1,28 @@
+
+
+my_numbers = [1, 2, 3, 5]
+my_numbers[3] = 4
+print(my_numbers)
+
+
+# লিস্টের যোগ ও গুন
+
+first_list = [1, 2, 3]
+print(first_list + [4, 5, 6])
+print(first_list * 3)
+
+
+# লিস্টের মধ্যের এলিমেন্ট চেক
+
+fruits = ["apple", "orange", "pineappe", "grape"]
+
+print("orange" in fruits)
+print("rice" in fruits)
+print("apple" in fruits)
+
+
+
+fruits = ["apple", "orange", "pineappe", "grape"]
+
+print("orange" not in fruits)
+print(not "rice" in fruits)
